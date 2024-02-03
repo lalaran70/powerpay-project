4210481f2dc4145f81f0c996aaeb1d73d49213c2
Your Name <you@example.com>
2024-02-02 20:40:48 -0600

    first commit

---
+++ demopowerpay-project/src/main/java/com/jendareypaymentproject/demopowerpayproject/DemopowerpayProjectApplication.java
@@ -0,0 +1,13 @@
+package com.jendareypaymentproject.demopowerpayproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemopowerpayProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemopowerpayProjectApplication.class, args);
+	}
+
+}
---
+++ demopowerpay-project/src/test/java/com/jendareypaymentproject/demopowerpayproject/DemopowerpayProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jendareypaymentproject.demopowerpayproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemopowerpayProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
